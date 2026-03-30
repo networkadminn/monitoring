b889e5b608df65b7600696cb94d0dc1bd8a8f196
networkadminn <networkadmin@euclideesolutions.com>
2026-03-30 10:32:57 +0530

    Cleanup: Remove test files

--- test_json.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('MONITOR_ROOT', __DIR__);
-require_once MONITOR_ROOT . '/config.php';
-require_once MONITOR_ROOT . '/includes/Database.php';
-$sites = Database::fetchAll('SELECT check_type FROM sites LIMIT 10');
-header('Content-Type: application/json');
-echo json_encode($sites);
