d0b321ececbfdc937ead9191c75be86b211dc4a8
networkadminn <networkadmin@euclideesolutions.com>
2026-03-30 11:17:08 +0530

    Fix HTTP 415 error by adding proper browser headers and removing unsupported Brotli encoding
    - Added complete browser headers (Accept, Accept-Language, Accept-Encoding, etc.)
    - Removed 'br' (Brotli) encoding as curl 7.29.0 doesn't support it
    - Set CURLOPT_ENCODING to 'gzip, deflate' for supported compression
    - Updated keyword check with same headers for consistency
    - Fixes issue where kaher.edu.in domains returned 415
    - Makes monitoring compatible with servers that require specific Accept headers

--- includes/Checker.php
+++ includes/Checker.php
@@ -72,12 +72,12 @@ class Checker {
             CURLOPT_HTTPHEADER     => [
                 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
                 'Accept-Language: en-US,en;q=0.9',
-                'Accept-Encoding: gzip, deflate, br',
+                'Accept-Encoding: gzip, deflate',
                 'Connection: keep-alive',
                 'Upgrade-Insecure-Requests: 1',
                 'Cache-Control: max-age=0'
             ],
-            CURLOPT_ENCODING       => '',  // Allow automatic decompression
+            CURLOPT_ENCODING       => 'gzip, deflate',  // Allow automatic decompression
         ]);
 
         $response     = curl_exec($ch);
@@ -202,7 +202,7 @@ class Checker {
         $context = stream_context_create(['http' => [
             'timeout'     => CHECK_TIMEOUT,
             'user_agent'  => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 SiteMonitor/1.1',
-            'header'      => "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.9\r\nAccept-Encoding: gzip, deflate, br\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\n",
+            'header'      => "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.9\r\nAccept-Encoding: gzip, deflate\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\n",
             'ignore_errors' => true,
         ]]);
 
